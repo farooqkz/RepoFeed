72cb3955a5ab18dbcca58a7af3a67694e11beb3b
Farooq Karimi Zadeh <farooqkz@hashbang.sh>
2021-05-03 18:19:04 +0200

    adding Event2HumanReadable class and many other things

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import hmac
 import codecs
+from dataclasses import dataclass
+from typing import Union
 
 from flask import Flask
 from flask import request
@@ -8,55 +10,55 @@ import lmdb
 
 app = Flask(__name__)
 
+LATEST_N = 12
 
-class Event2HR:
-    def __init__(self, json):
-        self.json = json
-        self.type = self._detectType()
+def detectType(json):
+
+@dataclass(eq=False)
+class Event2HumanReadable:
+    json: dict
+    type_: Union[str, NoneType]
+    type_func: dict = dict(issue=self.generate_for_issue, pull_request=self.generate_for_pr)
+
+    def __post_init__(self):
+        self.type_ = self._detectType()
 
     def _detectType(self):
-        types = (
-            "pull_request",
-            "pull_request_review",
-            "pull_request_review_comment",
-            "issues",
-            "fork",
-            "issue_comment",
-        )
-        for type_ in types:
-            if type_ in self.json:
-                return type_
-        return None
+        if "issue" in self.json:
+            return "issue"
+        elif "pull_request" in self.json:
+            return "pull_request"
+        return None # means unsupported type
 
     def generate_HR_text(self):
-        ...
-
-    def generate_for_issues(self):
+        if self.type_:
+            return self.type_func[self.type]()
+        return None
+    
+    def generate_for_issue(self):
         user = self.json["issue"]["user"]["login"]
         action = self.json["action"]
-        title = self.json["issue"]["title"]
-        body = self.json["issue"]["body"]
+        issue_title = self.json["issue"]["title"]
+        body = ""
+        comment = ""
+        if "comment" in self.json:
+            body = self.json["comment"].get("body", "")
+            comment = "comment"
+        else:
+            body = self.json["issue"].get("body", "")
 
-        return f'{user} {action} "{title}": {body}'
+        return f'{user} {action} {comment} for issue {issue_title}: {body}'
 
-    def generate_for_prs(self):
+    def generate_for_pr(self):
         user = self.json["pull_request"]["user"]["login"]
         action = self.json["action"]
-        if action == "closed" and self.json["merged"]:
+        if action == "closed" and self.json.get("merged", False):
             action = "merged"
-        title = self.json["pull_request"]["title"]
-        title = self.json["pull_request"]["body"]
+        title = self.json["pull_request"].get("title", "")
+        title = self.json["pull_request"].get("body", "")
 
         return f'{user} {action} "{title}": {body}'
 
-    def generate_for_prs_review(self):
-        ...
-
-    def generate_for_prs_review_comment(self):
-        ...
-
-    def generate_for_fork(self):
-        return ""
 
 
 def get_secret():
@@ -67,11 +69,10 @@ def get_secret():
     return secret
 
 
-def get_latest(n=10):
+def get_latest():
     with get_env().begin() as txn:
-        cursor = txn.cursor()
         count = txn.stat()["entries"]
-        start = max(count - n, 0)
+        start = max(count - LATEST_N, 0)
         for i in range(start, count):
             yield txn.get(f"{i}")
 
@@ -82,7 +83,17 @@ def get_env():
         env = g._env = lmdb.open("db")
     return env
 
-
+def add_action(json):
+    text = Event2HumanReadable(json).get_HR_text()
+    if not text:
+        return
+    
+    text = text.encode()
+    with get_env().begin() as txn:
+        count = txn.stat()["entries"]
+        txn.put(str(count+1).encode(), text)
+        txn.delete(str(count - LATEST_N).encode())
+        
 @app.route("/ghrss", methods=("POST",))
 def hook():
     if not request.is_json:
@@ -94,8 +105,12 @@ def hook():
             hmac.digest(get_secret(), request.get_data(), "sha256"), "hex"
         ).decode()
         if hex_digest1 == hex_digest0:
-            print(request.json)
+            add_action(request.json)    
         else:
             print("digests are not equal:", hex_digest1, hex_digest0)
 
     return dict(ok="O.K.")
+
+@app.route("/ghrss/feed", methods=("GET",))
+def feed():
+
